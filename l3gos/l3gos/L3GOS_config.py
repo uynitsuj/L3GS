a9041728b3b2e6607e44204787cef0ace6c5145c
Justin Yu <yujustin8@gmail.com>
2023-11-27 22:07:14 -0800

    pip install pyproject toml

--- l3gos/l3gos/L3GOS_config.py
+++ l3gos/l3gos/L3GOS_config.py
@@ -1,5 +1,5 @@
 """
-LERF configuration file.
+L3GOS configuration file.
 """
 
 from nerfstudio.cameras.camera_optimizers import CameraOptimizerConfig
@@ -9,9 +9,11 @@ from nerfstudio.engine.optimizers import AdamOptimizerConfig, RAdamOptimizerConf
 from nerfstudio.engine.schedulers import ExponentialDecaySchedulerConfig
 from nerfstudio.plugins.types import MethodSpecification
 from nerfstudio.models.base_model import ModelConfig
+from nerfstudio.data.datamanagers.full_images_datamanager import FullImageDatamanagerConfig
 from nerfstudio.models.gaussian_splatting import GaussianSplattingModelConfig
-from LifelongLEGOS.lifelong_legos.l3gos.L3GOS_trainer import TrainerConfig
-from L3
+from l3gos.L3GOS_trainer import TrainerConfig
+from l3gos.L3GOS_pipeline import L3GOSPipelineConfig
+from l3gos.data.L3GOS_dataparser import L3GOSDataParserConfig
 
 
 l3gos_method = MethodSpecification(
@@ -24,9 +26,9 @@ l3gos_method = MethodSpecification(
         max_num_iterations=30000,
         mixed_precision=False,
         gradient_accumulation_steps = {'camera_opt': 100,'color':10,'shs':10},
-        pipeline=VanillaPipelineConfig(
+        pipeline=L3GOSPipelineConfig(
             datamanager=FullImageDatamanagerConfig(
-                dataparser=ColmapDataParserConfig(load_3D_points=True),
+                dataparser=L3GOSDataParserConfig(),
             ),
             model=GaussianSplattingModelConfig(),
         ),
